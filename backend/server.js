865d74d1a7ba934298bcff27a1476832af5fa892
Jan Knaebe <jm.knaebe@gmail.com>
2025-09-14 17:50:16 +0200

    setup server.js

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Datenbank vebunden'))
+.catch(err => console.error('Verbindungsfehler:', err));
+
+app.get('/', (req, res) => {
+    res.send('Backend läuft');
+});
+
+const PORT = process.env.PORT || 5000; //Absicherung falls .env PORT nicht gesetzt ist
+app.listen(PORT, () => console.log('Server läuft auf Port: $(PORT)'));
