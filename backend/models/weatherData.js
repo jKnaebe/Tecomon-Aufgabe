c25616c713076355c0c06b2c0466a38d61e20fb9
Jan Knaebe <jm.knaebe@gmail.com>
2025-09-17 13:08:10 +0200

    created the model

---
+++ backend/models/weatherData.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const weatherSchema = new mongoose.Schema ({
+    city: {type: String, require: true},
+    temperature: Number,
+    condition: String,
+    updateedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('weathterData', weatherSchema);
